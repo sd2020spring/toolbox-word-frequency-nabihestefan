dc2e880347bfd8a376f6cd16f9337302a59b1cf7
nabihestefan <estefan.nabih@gmail.com>
2020-02-25 15:52:54 -0500

    Finished implementation of toolbox

--- frequency.py
+++ frequency.py
@@ -3,17 +3,34 @@
 import string
 
 
-def get_word_list(file_name):
+def getWordList(filename):
     """Read the specified project Gutenberg book.
 
     Header comments, punctuation, and whitespace are stripped away. The function
     returns a list of the words used in the book as a list. All words are
     converted to lower case.
     """
-    pass
+    temp = ""
+    i = 0
+    file = open(filename)
+    words = []
+    for line in file:
+        line.strip()
+        l = len(line)
+        i = 0
+        while i < l:
+            if line[i] in string.punctuation or line[i] in string.whitespace:
+                if(temp != ""):
+                    words.append(temp)
+                    temp = ""
+            else:
+                temp += line[i].lower()
+            i+=1
+
+    return(words)
 
 
-def get_top_n_words(word_list, n):
+def getTopNWords(words, n):
     """Take a list of words as input and return a list of the n most
     frequently-occurring words ordered from most- to least-frequent.
 
@@ -31,9 +48,26 @@ def get_top_n_words(word_list, n):
         The n most frequently occurring words ordered from most to least.
     frequently to least frequentlyoccurring
     """
-    pass
+    d = dict()
+    for word in words:
+        d[word] = d.get(word, 0) + 1
+    s = sorted(d.items(), key=lambda x: x[1], reverse=True)
+    top = []
+    for i in range(n):
+        t = s[i]
+        top.append(t[0])
+    return(top)
+
 
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
     print(string.punctuation)
+    print("Inferno")
+    print(getTopNWords(getWordList("Inferno"), 10))
+    print()
+    print("Candide")
+    print(getTopNWords(getWordList("Candide"), 10))
+    print()
+    print("CompleteShakespeare")
+    print(getTopNWords(getWordList("CompleteShakespeare"), 10))
